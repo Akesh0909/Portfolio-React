55c49e039e4435ae8154f84c6dcb98723a01bbf6
Akesh <85454169+Akesh0909@users.noreply.github.com>
2021-11-15 12:40:28 +0530

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -32,7 +32,7 @@ class Contact extends Component {
         <div className="row">
           <Slide left duration={1000}>
             <div className="eight columns">
-              <form action="" method="post" id="contactForm" name="contactForm">
+              <form action="https://formspree.io/f/xoqygwko" method="post" >
                 <fieldset>
                   <div>
                     <label htmlFor="contactName">
